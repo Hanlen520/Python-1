dc24ce1710799b5e643bb3c7ee3653cbc13074b1
Crisimple <1240281595@qq.com>
2019-01-03 23:17:58 +0800

    init local enviroment

--- studycodes/21email.py
+++
@@ -1,7 +0,0 @@
-#!/bin/usr/env python3
-# -*- coding: utf-8 -*-
-# filename: 21email.py
-# author:
-# description:
-
-# 电子邮件在互联网上是如何运作的
--- studycodes/coreProgramming/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/bin/usr/env python3
-# -*- coding: utf-8 -*-
-# filename: .py
-# author: jiangheng
-# description:
-# questions:
