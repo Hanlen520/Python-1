f72d8428e47af75baa4ca4cd32d1c4ef37dc3eb9
Crisimple <1240281595@qq.com>
2019-01-06 23:00:07 +0800

    dict operator

--- OldBoyPython/D6_02字典操作和内置方法.py
+++ OldBoyPython/D6_02字典操作和内置方法.py
@@ -41,8 +41,8 @@ f = dict_f.pop("job")
 print("pop删除指定值返回：", f)
 print("pop指定键删除字典的值：", dict_f)
 print("popitem随机删除字典的值：", dict_f.popitem())
-del dict_f
-print("del删除整个字典dict_f：", dict_f)
+# del dict_f
+# print("del删除整个字典dict_f：", dict_f)
 
 # 改
 dict_b = {"name": "B", "age": 24, "hobby": 'jita', "job": 'IT'}
@@ -54,10 +54,56 @@ print("如果本来存在键，setdefault返回值：", b1)
 b2 = dict_b.setdefault("single", True)
 print("本来不存在键，setdefault返回值：", b2)
 
-
 # 查
 print("通过字典的键来查：", dict_a["name"])
 print("通过keys查看所有的键：", dict_a.keys())
 print("通过values查看所有的值：", dict_a.values())
 print("通过items查看所有的键值对：", dict_a.items())
+
+
+# 字典的内置方法
+# fromkeys
+dict_g = dict.fromkeys(["name1", "name2", "name3"], "AA")
+print("fromkeys的全赋值情况一：", dict_g)
+dict_h = dict.fromkeys(["name4", "name5", "name6"], ["B", "c"])
+print("fromkeys的全赋值情况二：", dict_h)
+# ***此处涉及到深浅拷贝的知识点***
+dict_h["name5"][1] = "X"
+print(dict_h)
+
+# 字典的嵌套
+video_sites = {
+    "哔哩哔哩": {
+        "电影1": ["我在故宫修文物", "上车，走吧", "本命年1990", "横空出世1999", "胜利大阅兵"],
+        "电视剧1": ["情满四合院", "琅琊榜", "人民的名义", "猎场"],
+        "动画1": ["那年那事那些花", "你的名字", "一禅小和尚"]
+    },
+    "爱奇艺": {
+        "电影2": ["钢铁侠"],
+        "电视剧2": ["正阳门下"],
+        "动画2": ["龙猫"]
+    }
+}
+print(video_sites["哔哩哔哩"]["电影1"][1])
+
+# 排序
+print(sorted(video_sites))
+print(sorted(video_sites.items()))
+# print(sorted(video_sites.values())
+
+# 拷贝
+dict_i = {
+    "a": 1,
+    "b": 2,
+    "c": 3
+}
+# dict_copy = dict_i.copy()
+# print("字典拷贝：", dict_copy)
+
+# 遍历
+for i in dict_i:
+    print(i, dict_i[i])
+
+for i, j in dict_i.items():
+    print(i, j)
 
---
+++ OldBoyPython/D6_03字符串的操作.py
@@ -0,0 +1,10 @@
+#!/bin/usr/env python3
+# -*- coding: utf-8 -*-
+# --------------------------------------
+# ProjectName: MySpace
+# Author: crisimple
+# CreateTime: 2019/1/5 17:32
+# FileName: D6_03字符串的操作.py
+# Description: 
+# Question:
+# --------------------------------------
