cb60e00eeccb92c22251e8cf32dd6c106eddff84
Crisimple <1240281595@qq.com>
2019-01-08 00:40:34 +0800

    string is end and the three menu start

---
+++ OldBoyPython/D6_04灵活的三级菜单.py
@@ -0,0 +1,10 @@
+#!/bin/usr/env python3
+# -*- coding: utf-8 -*-
+# --------------------------------------
+# ProjectName: MySpace
+# Author: crisimple
+# CreateTime: 2019/1/8 0:39
+# FileName: D6_04灵活的三级菜单.py
+# Description: 
+# Question:
+# --------------------------------------
