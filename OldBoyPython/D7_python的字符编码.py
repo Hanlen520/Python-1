917d6424711cdb7ac24904fcd31126bf128403cb
Crisimple <1240281595@qq.com>
2019-01-09 08:05:02 +0800

    unicode

---
+++ OldBoyPython/D7_python的字符编码.py
@@ -0,0 +1,25 @@
+#!/bin/usr/env python3
+# -*- coding: utf-8 -*-
+# --------------------------------------
+# ProjectName: MySpace
+# Author: crisimple
+# CreateTime: 2019/1/8 22:18
+# FileName: D7_python的字符编码.py
+# Description: 
+# Question:
+# --------------------------------------
+
+# 二进制
+# 美国标准
+# -----ASCII: 只能存英文和拉丁字符， 一个字符占一个字节(8位)
+# 国标
+# -----gb2312: 只能存6700多个中文，1980
+# -------gbk1.0: 存20000多字符， 1995
+# ---------gb18030: 27000中文
+# 万国码
+# -----Unicode：utf-32   一个字符占4个字节
+# -----Unicode：utf-16   一个字符占2个字节
+# -----Unicode：utf-8    一个英文用ASCII码来存，一个中文占3个字节
+
+# 编码encode: 将语言转换为Unicode
+# 解码decode: 将Unicode转换为某种语言
