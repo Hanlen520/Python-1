fbf128dd0c9d03c187b9b7b20e360351517b60b6
Crisimple <1240281595@qq.com>
2019-01-09 23:26:43 +0800

    add file operator

---
+++ OldBoyPython/D8_文件操作基本流程.py
@@ -0,0 +1,10 @@
+#!/bin/usr/env python3
+# -*- coding: utf-8 -*-
+# --------------------------------------
+# ProjectName: MySpace
+# Author: crisimple
+# CreateTime: 2019/1/9 23:26
+# FileName: D8_文件操作基本流程.py
+# Description: 
+# Question:
+# --------------------------------------
