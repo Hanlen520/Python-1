569415736c1f705973d391886b7ba1148619d1ce
Crisimple <1240281595@qq.com>
2019-01-21 22:39:09 +0800

    test

--- OldBoyPython/D10_05迭代器.py
+++ OldBoyPython/D10_05迭代器.py
@@ -10,9 +10,33 @@
 # --------------------------------------
 
 # 生成器都是迭代器，迭代器不一定是生成器
+from collections.abc import Iterable
 
 
 # 1. 什么时可迭代对象
 #   for 循环后面跟的都是可迭代对象
 #   可迭代对象都有__iter__方法
-#   常见的几种数据类型中：列表、元组、字典均是可迭代对象
+#   常见的几种数据类型中：列表、元组、字典、字符串均是可迭代对象
+list_a = [1, 2, 3, 4]
+print(list_a.__iter__())
+a = iter(list_a)
+print(a)
+
+
+# 2. 什么是迭代器
+#   一、有iter()方法
+#   二、有next()方法
+print(next(a))
+print(next(a))
+
+
+# 3. for进行生成器或迭代器循环时，进行了三件事：
+# （1）调用可迭代对象的iter方法返回一个可迭代对象
+# （2）不断调用迭代器的next方法
+# （3）处理StopIteration
+# for i in list_a:
+#     iter(i)     # TypeError: 'int' object is not iterable
+
+
+# 4. 判断是否是迭代器
+print(isinstance(a, Iterable))
---
+++ OldBoyPython/D11_01模块time.py
@@ -0,0 +1,10 @@
+#!/bin/usr/env python3
+# -*- coding: utf-8 -*-
+# --------------------------------------
+# ProjectName: MySpace
+# Author: crisimple
+# CreateTime: 2019/1/20 17:37
+# FileName: D11_01模块time.py
+# Description: 
+# Question:
+# --------------------------------------
