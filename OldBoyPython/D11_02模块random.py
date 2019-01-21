e613e51b2d05105916bce5f3f542e9c5d1aaf604
unknown <jiangheng@ESG.360ES.CN>
2019-01-21 16:40:20 +0800

    D11_02

---
+++ OldBoyPython/D11_02模块random.py
@@ -0,0 +1,10 @@
+#!/bin/usr/env python3
+# -*- coding: utf-8 -*-
+# --------------------------------
+# ProjectName: MySpace
+# Author: crisimple
+# CreateTime: 2019/1/21 16:39
+# FileName: D11_02模块random.py
+# Description: 
+# Question: 
+# --------------------------------
