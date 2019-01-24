f496d501e27ec888d48e3e83164bd3c5ad8f9f03
unknown <jiangheng@ESG.360ES.CN>
2019-01-24 17:07:54 +0800

    re

--- OldBoyPython/D11_08模块正则re.py
+++ OldBoyPython/D11_08模块正则re.py
@@ -5,6 +5,22 @@
 # Author: crisimple
 # CreateTime: 2019/1/23 23:58
 # FileName: D11_08模块正则re.py
-# Description: 
+# Description:
+#           就其本质而言，正则表达式(或re)是一种小型的、高度专业化的编程语言，它内嵌在Python中
+#       并通过re模块来实现，正则表达式模式被编译成一系列的字节码，然后由用C编写的匹配引擎执行。
+#  正则的作用：可以进行模糊匹配
 # Question:
 # --------------------------------------
+
+string1 = "Hello Python"
+
+# 1. 字符串匹配
+# 字符串提供的方法是一种完全匹配，正则提供的是模糊匹配
+string1.find()
+
+
+
+
+
+
+
