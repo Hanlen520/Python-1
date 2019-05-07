c1ce978b39d250c745a8e4343ccd6a20665b5402
unknown <jiangheng@ESG.360ES.CN>
2019-05-07 21:09:53 +0800

    json

--- InterfaceTest/04Serialization.py
+++ InterfaceTest/04Serialization.py
@@ -11,5 +11,22 @@
 
 import json
 
-print()
+print('json_methods: ', json.__all__)
 
+
+dict1 = {
+    'name': 'A',
+    'age': 25,
+    'job': 'IT'
+}
+def dict_to_json():
+    print('dict1_origin_type: ', type(dict1))
+    print('dict1_origin: ', dict1)
+
+    json1 = json.dumps(dict1)
+    print('json1_type: ', type(json1))
+    print('json1: ', json1)
+dict_to_json()
+
+def json_to_dict():
+    pass
